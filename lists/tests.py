33f53db5a5b715b2bb7db0aea91d85970663f4d2
Vitalij Ivanovskij <vivanovskijj@gmail.com>
2021-08-06 22:10:56 +0300

    Added application with intentionally not working test

---
+++ lists/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+class SmokeTest(TestCase):
+    '''тест на токсичность'''
+
+    def test_bad_maths(self):
+        '''тест: неправильные математ. расчеты'''
+        self.assertEqual(1 + 1, 3)
