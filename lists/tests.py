d6c14ae86c160dedb760d21a42adbdcede100ce2
Vitalij Ivanovskij <vivanovskijj@gmail.com>
2021-08-06 22:42:23 +0300

    first unit test and transformation url, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 # Create your tests here.
 
 class SmokeTest(TestCase):
     '''тест на токсичность'''
 
-    def test_bad_maths(self):
-        '''тест: неправильные математ. расчеты'''
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        '''тест: корневой url преобразуется в представление'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_page(self):
+    '''домашняя страница'''
+    pass
