e06465c2d03d82d1513b08b2c73e7c56ebf31f89
Vitalij Ivanovskij <vivanovskijj@gmail.com>
2021-08-08 18:52:45 +0300

    Refactoring home_page view fot using template

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,8 @@
+from superlists.settings import SECRET_KEY
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
-from superlists.settings import SECRET_KEY
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -23,9 +24,11 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         '''тест: домашняя страница возвращает правильный html'''
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
+
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
