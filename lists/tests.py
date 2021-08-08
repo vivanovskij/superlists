f03f114ff1e48f9f5e15858a4f2e9eb89d565955
Vitalij Ivanovskij <vivanovskijj@gmail.com>
2021-08-08 13:20:28 +0300

    Basic view return minimal html now

--- lists/tests.py
+++ lists/tests.py
@@ -1,13 +1,31 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+from superlists.settings import SECRET_KEY
+
 from lists.views import home_page
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
-    '''тест на токсичность'''
+class HomePageTest(TestCase):
+    '''тест домашней страницы'''
 
     def test_root_url_resolves_to_home_page_view(self):
         '''тест: корневой url преобразуется в представление'''
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_secret_key(self):
+        '''тест: соответствие SECRET_KEY'''
+        self.assertEqual( \
+             'django-insecure-7)6!^4h%xv&z$&@bl6g)rj5zy^+7*z&!*3+h73k5tf$zs0w1if', \
+             SECRET_KEY)
+
+    def test_home_page_returns_correct_html(self):
+        '''тест: домашняя страница возвращает правильный html'''
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- superlists/settings.py
+++ superlists/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7)6!^4h%xv&z$&@bl6g)rj5zy^+7*z&!*3+h73k5tf$zs0w1if'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
