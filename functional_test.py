913ce015f912dc95e1ebd102f063e5f6292320a0
Vitalij Ivanovskij <vivanovskijj@gmail.com>
2021-08-08 15:53:16 +0300

    Functional test check abbility to enter element

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,23 +19,41 @@ class NewVisitorTest(unittest.TestCase):
 
          # Оксана слышала про крутое новое онлайн-приложение со спиком
          # неотложных дел. Она решает оценить его домашнюю страницу
-
         self.browser.get('http://localhost:8000')
 
         # Она видит, что заголовок и шапка страницы говорят о списках неотложных дел
-
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = \
+            self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
 
         # Ей сразу же предлагается ввести элемент списка
+        inputbox = \
+            self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби - вязание рыболовных мушек)
+        inputbox.send_keys('Купить павлиньи перья')
 
         # Когда она нажимает enter, страница обновляется, и теперь страница содержит "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table
+        self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+             any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент. Она вводит "Сделать мушку из павлиньих перьев".
-
         # (Оксана очень методична)
+
+        self.fail('Закончить тест!')
 
         # Страница снова обновляется, и теперь показывает оба элемента списка
 
